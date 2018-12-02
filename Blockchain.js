851ae09afdc81261f2c717b8c7e4deb58167eb6e
Theaisback <alanhekle@hotmail.com>
2018-12-01 20:15:52 -0600

    removed a lot of files

--- Blockchain.js
+++ Blockchain.js
@@ -1,34 +1,22 @@
 /*==========================	SHA256 with Crypto-js	============================|
-|						Learn more: Crypto-js: https://github.com/brix/crypto-js						|
+|						Learn more:	Crypto-js:	https://github.com/brix/crypto-js						|
 |==============================================================================*/
-
 const SHA256 = require('crypto-js/sha256');
-
-/*==========================	Adding Block.js	==================================|
-|										Class with a constructor for block													|
-|==============================================================================*/
-
-const Block = require('./Block.js');
-
 /*========================	Persist data with LevelDB	==========================|
-|							 Learn more: level: https://github.com/Level/level								|
+|								Learn more: level: https://github.com/Level/level								|
 |==============================================================================*/
-
 const level = require('level');
 const chainDB = './chaindata';
 const db = level(chainDB);
-
 /*==============================	Functions	====================================|
 |															List of Functions																	|
 |==============================================================================*/
-
 //	Add data to levelDB with key/value pair
 function addLevelDBData(key, value) {
 	db.put(key, JSON.stringify(value), function(err) {
 		if (err) return console.log('Block ' + key + ' submission failed', err);
 	});
 }
-
 //	Get data from levelDB with key
 function getLevelDBData(key) {
 	return new Promise((resolve, reject) => {
@@ -42,24 +30,21 @@ function getLevelDBData(key) {
 		});
 	});
 }
-
-//	Add data to levelDB with value
-function addDataToLevelDB(value) {
-	let i = 0;
-	db.createReadStream().on('data', function(data) {
-		i++;
-	}).on('error', function(err) {
-		return console.log('Unable to read data stream!', err)
-	}).on('close', function() {
-		console.log('Block #' + i);
-		addLevelDBData(i, value);
-	});
-}
-
-/*=============================	Blockchain Class	==============================|
-|									 Class with a constructor for new blockchain 				 					|
+/*==============================	Block Class	==================================|
+|											Class with a constructor for Block												|
 |==============================================================================*/
-
+class Block {
+	constructor(data){
+		this.hash		=	"",
+		this.height	=	0,
+		this.body		=	data,
+		this.time		= 0,
+		this.previousHash = ""
+	}
+}
+/*=============================	Blockchain Class	==============================|
+|										Class with a constructor for new blockchain									|
+|==============================================================================*/
 class Blockchain {
 	constructor() {
 		let height = this.getBlockHeight();
@@ -68,7 +53,6 @@ class Blockchain {
 			this.addBlock(new Block("First block in the chain - Genesis block"));
 		}
 	}
-
 	//	Add new block
 	//	addBlock(newBlock) includes a method to store newBlock within LevelDB
 	addBlock(newBlock) {
@@ -90,14 +74,12 @@ class Blockchain {
 		//	Add the newblock to the function addDataToLevelDB, new way to get height		
 		this.addLevelDBData(newBlock.height, newBlock);
 	}
-
 	//	New method to add information to LevelDB.
 	addLevelDBData(height, newBlock) {
 		db.put(height, JSON.stringify(newBlock), function(err) {
 			if (err) return console.log('Block ' + newBlock.height + ' submission failed', err);
 		});
 	}
-
 	//	Modify getBlockHeight() function to retrieve current block height within the LevelDB chain
 	getBlockHeight() {
 		let i = 0;
@@ -110,7 +92,6 @@ class Blockchain {
 			return i -1;
 		});
 	}
-
 	//	Modify getBlock() function to retrieve a block by its block height within the LevelDB chain
 	getBlock(blockHeight) {
 		getLevelDBData(blockHeight)
@@ -165,20 +146,52 @@ class Blockchain {
 	}
 }
 
-/*=================================	Testing	====================================|
-|	-	Self-invoking function to add blocks to chain																|
-|	-	Learn more:																																	|
-|		https://scottiestech.info/2014/07/01/javascript-fun-looping-with-a-delay/		|
-|																																								|
-|	*	100 Milliseconds loop = 36,000 blocks per hour															|
-|			(`13.89 hours for 500,000 blocks )																				|
-|	Bitcoin blockchain adds 8640 blocks per day																		|
-|			(	new block every 10 minutes )																						|
-|==============================================================================*/
+const express = require('express')
+var bodyParser = require('body-parser')
 
-(function theLoop (i) {
-	setTimeout(function () {
-		addDataToLevelDB('Testing data');
-		if (--i) theLoop(i);
-	}, 100);
-})(10);
+const app = express()
+const port = 8000
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//	Get block endpoint
+app.get('/block/:blockheight', (req, res) => {
+	let blockchain = new simpleChain.Blockchain();
+	let blockheight = req.params.blockheight
+	//	console.log("Blockheight requested is", blockheight)
+	blockchain.getBlock(blockheight)
+		.then(block => {
+			res.send(block)
+		}, err => {
+			res.send({
+				status: 'error',
+				message: 'block not found'
+			})
+		})
+})
+//	Post block endpoint
+app.post('/block', (req, res) => {
+	let blockchain = new simpleChain.Blockchain();
+	let blockData = req.body
+	//	console.log(blockData)
+	if(blockData.body.length > 0) {
+		//	console.log("block data is", blockData)
+		let newBlock = new simpleChain.Block(blockData.body)
+		blockchain.addBlock(newBlock)
+			.then(block => {
+				res.send(block)
+			}, err => {
+				res.send({
+					status: 'error',
+					message: err
+				})
+			})
+		} else {
+			res.send({
+				status: 'error',
+				message: 'block data is missing'
+			})
+		}
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
